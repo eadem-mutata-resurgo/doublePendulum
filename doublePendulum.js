773ba85dbaf41ccaec9e5367ae949de9616b981c
Jacob Riceman Chappell <93935794+eadem-mutata-resurgo@users.noreply.github.com>
2023-11-07 12:07:00 -0600

    updated aesthetics

--- doublePendulum.js
+++ doublePendulum.js
@@ -14,6 +14,10 @@ const p2AngleSlider = document.getElementById("p2AngleSlider");
     p2AngleSlider.addEventListener("input", reset);
 const p2MassSlider = document.getElementById("p2MassSlider");
     p2MassSlider.addEventListener("input", reset);
+const tailLengthSlider = document.getElementById("tailLengthSlider");
+    tailLengthSlider.addEventListener("input", tailUpdate);
+const infTailSwitch = document.getElementById("infTailSwitch");
+    infTailSwitch.addEventListener("input", tailUpdate);
 
 const w = canvas.width;
 const h = canvas.height;
@@ -23,19 +27,17 @@ let running = false;
 const g = 1;            //gravitational constant
 const dt = 0.05;        //time step
 const l = [1.25, 1.25];       //length of pendulums: [l1, l2]
-let m, ang, vel;
-let prevAng;
+let m, ang, prevAng, vel, maxVel;
 
-let tailLength = 255;
-let tailAngVals = [];
-
-let tailCol = [50, 255, 255];
+let tailLength, tailAngVals, curAng;
+let tailCol = [255, 100, 0];
 
 window.onload = (event) => {
     context.scale(1, -1)
     context.translate(w/2, -h/2);
     context.scale(w/(2*axMax), -h/(2*axMax));
-    context.fillStyle = "blue";
+    curAng = [];
+    maxVel = [1, 1];
     reset();
 }
 
@@ -44,15 +46,20 @@ function reset() {
     m = [p1MassSlider.value, p2MassSlider.value];
     ang = [p1AngleSlider.value*pi/180, p1AngleSlider.value*pi/180 + p2AngleSlider.value*pi/180];
     prevAng = [p1AngleSlider.value*pi/180, p1AngleSlider.value*pi/180 + p2AngleSlider.value*pi/180];
-    vel = [0, 0];    
-    tailAngVals = [];
-    for (let i = 0; i < tailLength; i++) {
-        tailAngVals[i] = [l[1]*sin(ang[1]) + l[0]*sin(ang[0]), l[1]*cos(ang[1]) + l[0]*cos(ang[0])];
-    }
+    vel = [0, 0];
+    tailUpdate();
     context.clearRect(-axMax, -axMax, 2*axMax, 2*axMax);
     drawAxes();
     drawPendulums();
 
+}
+
+function tailUpdate() {
+    tailLength = tailLengthSlider.value;
+    tailAngVals = [];
+    for (let i = 0; i < tailLength; i++) {
+        tailAngVals[i] = [l[1]*sin(ang[1]) + l[0]*sin(ang[0]), l[1]*cos(ang[1]) + l[0]*cos(ang[0])];
+    }
 }
 
 function startStop() {
@@ -80,8 +87,15 @@ function drawPendulums() {
     let p2 = [l[1]*sin(ang[1]) + p1[0], l[1]*cos(ang[1]) + p1[1]];
 
     context.save();
-    context.strokeStyle = "purple";
+    context.strokeStyle = "black";
+    context.fillStyle = "black";
     context.lineWidth = 6*axMax/w;
+
+    //anchor
+    context.beginPath();
+    context.arc(0, 0, 8*axMax/w, 0, 2*pi);
+    context.fill();
+    context.closePath();
 
     //bars
     context.beginPath();
@@ -92,10 +106,19 @@ function drawPendulums() {
     context.closePath();
 
     //weights
+    context.lineWidth = 4*axMax/w;
     context.beginPath();
-    context.arc(p1[0], p1[1], (m[0]/5000)**(1/3), 0, 2*pi);
-    context.arc(p2[0], p2[1], (m[1]/5000)**(1/3), 0, 2*pi);
+    context.fillStyle = colorGrad([0, 0, 255], [255, 0, 0], Math.abs(vel[0])/maxVel[0]);
+    context.arc(p1[0], p1[1], (m[0]*axMax/w/5)**(1/3), 0, 2*pi);
     context.fill();
+    context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.fillStyle = colorGrad([0, 0, 255], [255, 0, 0], Math.abs(vel[1])/maxVel[1]);
+    context.arc(p2[0], p2[1], (m[1]*axMax/w/5)**(1/3), 0, 2*pi);
+    context.fill();
+    context.stroke();
     context.closePath();
 
     context.restore();
@@ -117,33 +140,25 @@ function drawAxes() {
     context.stroke();
     context.closePath();
 
-
-    //axes
-    context.strokeStyle = "black";
-    context.lineWidth = 3*axMax/w;
-    context.beginPath();
-    context.moveTo(-axMax, 0);
-    context.lineTo(axMax, 0);
-    context.moveTo(0, -axMax);
-    context.lineTo(0, axMax);
-    context.stroke();
-    context.closePath();
-
     context.restore();
 }
 
 function drawTail() {
     tailAngVals.push([l[1]*sin(ang[1]) + l[0]*sin(ang[0]), l[1]*cos(ang[1]) + l[0]*cos(ang[0])]);
-    tailAngVals.shift();
-
+    if (!infTailSwitch.checked) { tailAngVals.shift(); }
     context.save();
-
-    for (let i=1; i<tailLength; i++ ) {
+    context.lineWidth = 2*axMax/w;
+    for (let i=1; i<tailAngVals.length; i++ ) {
         context.beginPath();
 
-        context.strokeStyle = colorGrad([255, 255, 255], tailCol, i/tailLength);
-        context.lineWidth = (4*axMax/w) * i/tailLength;
-
+        
+        if (!infTailSwitch.checked) {
+            context.lineWidth = (4*axMax/w) * i/tailLength;
+            context.strokeStyle = colorGrad([255, 255, 255], tailCol, i/tailLength);
+        }
+        else  {
+            context.strokeStyle = colorGrad([255, 255, 255], tailCol, 1);
+        }
         context.moveTo(tailAngVals[i-1][0], tailAngVals[i-1][1]);
         context.lineTo(tailAngVals[i][0], tailAngVals[i][1]);
         context.stroke();
@@ -162,8 +177,6 @@ function colorGrad(c1, c2, d) {
 
 //math
 function compute() {
-
-    let curAng = [];
     curAng[0] = ang[0];
     curAng[1] = ang[1];
 
@@ -176,6 +189,8 @@ function compute() {
     prevAng[0] = curAng[0];
     prevAng[1] = curAng[1];
 
+    Math.abs(vel[0]) > maxVel[0] ? maxVel[0] = Math.abs(vel[0]) : void(0);
+    Math.abs(vel[1]) > maxVel[1] ? maxVel[1] = Math.abs(vel[1]) : void(0);
 }
 
 function motionEq1(a1, a2, v1, v2) {
